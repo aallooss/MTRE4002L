7a217788010b813bb5bedd5ea13bbb7fcad01a05
aallooss <44009215+aallooss@users.noreply.github.com>
2022-03-04 12:31:22 -0500

    Final Commit

--- Lab0/Source/Question_1.m
+++ Lab0/Source/Question_1.m
@@ -2,17 +2,20 @@ clc;
 clear all;
 close all;
 
+R=30; %Resistance
+C=0.0237; %Capacitance
 dt=0.01; %time step size
 t=0:dt:10; %simulation time
-v=0.1*sin(2*pi*t); %input voltage
-u=ones(1,length(t)); %u(t) is the step input
-dy(1)=0;
-y(1)=0;
+v=.1*sin(2*pi*t); %input voltage
+q(1)=0; %setting initial condition
 
-for i=1:length(t)
-    ddy(i)=u(i) - 3*dy(i) - 2*y(i);
-    dy(i+1)=ddy(i)*dt + dy(i);
-    y(i+1)=dy(i)*dt + y(i);
+for i=1:length(t) %Euler's method
+    dq(i)=v(i)/R-q(i)/(R*C);
+    q(i+1)=dq(i)*dt + q(i);
 end
-i=ddy;
-plot(t,i)
+
+i=dq; %current
+plot(t,i) % ploting current vs. time
+xlabel('Time (s)','FontSize',12,'FontWeight','bold','Color','b') 
+ylabel('Current (A)','FontSize',12,'FontWeight','bold','Color','b')
+title('RC Circuit','FontSize',12,'FontWeight','bold')
--- Lab0/Source/Question_5.m
+++ Lab0/Source/Question_5.m
@@ -2,46 +2,43 @@ clc;
 clear all;
 close all;
 
-dt=0.1; %time step size
+dt=0.01; %time step size --- 0.5s then 0.1s then 0.01s
 t=0:dt:10; %simulation time
-%v=0.1*sin(2*pi*t); %input voltage
 u=sin(2*pi*t); %u(t) is the sine input
-dy(1)=0;
-y(1)=(0);
+dy(1)=0; %initializing
+y(1)=(0); %initializing
 
-for i=1:length(t)
+for i=1:length(t) %Euler's method
     ddy(i)=u(i) - 3*dy(i) - 2*y(i);
     dy(i+1)=ddy(i)*dt + dy(i);
     y(i+1)=dy(i)*dt + y(i);
     z(i) = (((2*pi*exp(-t(i))) / (4*pi^2+1)) - ((pi*exp(-2*t(i))) / (2*pi^2+2)) - ((3*pi*cos(2*pi*t(i))-sin(2*pi*t(i))+2*pi^2*sin(2*pi*t(i))) / ((pi^2+1)*(8*pi^2+2)))) 
 end
-y(end)=[];
-i=y;
-error = z-y
-%z(end)=[];
-b=z;
 
-subplot(1,2,1)
-plot(t,i,'--ob')
-grid on
+y(end)=[];%deleting last element in vector
+error = z-y %calculate error between numerical and analytical
+
+subplot(1,2,1) %plot in first position
+plot(t,y,'--ob') %plotting y value vs. time
+grid on %turns plot grid marks on
 txt = ['\delta(t) = ' char(10) num2str(dt)];
-text(5,.25,txt)
-xticks(0:2:10)
+text(5,.25,txt) %adds text to center of plot
+xticks(0:2:10) %sets ticks to x axis
 xlabel('Time (s)','FontSize',12,'FontWeight','bold','Color','b') 
 ylabel('Y','FontSize',12,'FontWeight','bold','Color','b') 
 title('Sine Function','FontSize',12,'FontWeight','bold')
 
-hold on
+hold on %allows to plot two functions on one plot
 
-plot(t,b,'--*r')
+plot(t,z,'--*r') %plotting z value vs. time
 legend('Numerical Solution','Analytical Solution')
 
-hold off
+hold off %now plotting on seperate plot
 
-subplot(1,2,2)
-plot(t,error,'--ob')
-grid on
-xticks(0:2:10)
+subplot(1,2,2) %plot in second position
+plot(t,error,'--ob') %plotting error value vs. time
+grid on %turns plot grid marks on
+xticks(0:2:10) %sets ticks to x axis
 xlabel('Time (s)','FontSize',12,'FontWeight','bold','Color','b') 
 ylabel('error','FontSize',12,'FontWeight','bold','Color','b') 
 title('Sine Function','FontSize',12,'FontWeight','bold')
